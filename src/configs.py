743d81a43f91f591ced64d9d031df50218f34d0d
Bitupan Arandhara <bitupan_arandhara@Bitupans-MacBook-Air.local>
2025-11-03 22:05:39 +0530

    degradation script completed

--- src/configs.py
+++ src/configs.py
@@ -15,8 +15,15 @@ ORIGINAL_IMG_DIR = os.path.join(DATA_DIR, 'original_images')
 
 # --- 3. Degraded Data Paths ---
 DEGRADED_IMAGES_DIR = os.path.join(DATA_DIR, 'degraded_images')
-DEGRADED_NOISE_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'gaussian_noise')
-DEGRADED_BLUR_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur')
+DEGRADED_NOISE_LOW_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'gaussian_noise_low')
+DEGRADED_NOISE_MED_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'gaussian_noise_med')
+DEGRADED_NOISE_HIGH_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'gaussian_noise_high')
+DEGRADED_BLUR_LOW_HOR_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur_low_hor')
+DEGRADED_BLUR_LOW_VER_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur_low_ver')
+DEGRADED_BLUR_MED_HOR_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur_med_hor')
+DEGRADED_BLUR_MED_VER_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur_med_ver')
+DEGRADED_BLUR_HIGH_HOR_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur_high_hor')
+DEGRADED_BLUR_HIGH_VER_DIR = os.path.join(DEGRADED_IMAGES_DIR, 'motion_blur_high_ver')
 
 # --- 4. Restored Data Paths ---
 RESTORED_IMAGES_DIR = os.path.join(DATA_DIR, 'restored_images')
@@ -30,15 +37,20 @@ SIGMA_MED = 25
 SIGMA_HIGH = 50
 BLUR_ANLGE_HORIZONTAL = 0
 BLUR_ANGLE_VERTICAL = 90
-BLUR_ANGLE_DIAGONAL = 45
-BLUR_KERNEL_SIZE = 13
+BLUR_KERNEL_LOW = 3
+BLUR_KERNEL_MED = 9
+BLUR_KERNEL_HIGH = 15
 
 # --- 6. YAML File Paths for evaluation  ---
-ORIGINAL_YAML = '../data/original_validation.yaml'
-DEGRADED_NOISE_YAML = '../data/degraded_noise_validation.yaml'
-RESTORED_NOISE_YAML = '../data/restored_noise_validation.yaml'
+ORIGINAL_YAML = '../data/dataset_yaml/original_validation.yaml'
+DEGRADED_NOISE_YAML = '../data/dataset_yaml/degraded_noise_validation.yaml'
+RESTORED_NOISE_YAML = '../data/dataset_yaml/restored_noise_validation.yaml'
 DEGRADED_BLUR_YAML = '../data/dataset_yaml/degraded_blur_validation.yaml'
-RESTORED_BLUR_YAML = '../data/restored_blur_validation.yaml'
+RESTORED_BLUR_YAML = '../data/dataset_yaml/restored_blur_validation.yaml'
+
+# CREATE DATASET:
+# python create_degraded_datasets.py --type 'noise_low'
+# python create_degraded_datasets.py --type 'blur_low_hor'
 
 # Motion Deblur:
 # python restore.py --task Motion_Deblurring --input_dir '../data/degraded_images/motion_blur/' --result_dir '../data/restored_images/motion_deblur/'
--- src/create_degraded_datasets.py
+++ src/create_degraded_datasets.py
@@ -1,9 +1,11 @@
 import numpy as np
 import cv2
 from PIL import Image
-from matplotlib import pyplot as plt
 import os
 import configs
+import argparse
+import glob
+from tqdm import tqdm
 
 def custom_pil_imread(filepath):
     pil_img = Image.open(filepath)
@@ -19,9 +21,9 @@ def add_gaussian_noise(image, mean = configs.MEAN, sigma = configs.SIGMA_MED):
     noisy_image = image + gaussian_noise
     noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
 
-    return Image.fromarray(noisy_image)
+    return noisy_image
 
-def apply_motion_blur(image, kernel_size = configs.BLUR_KERNEL_SIZE, angle = configs.BLUR_ANLGE_HORIZONTAL):
+def apply_motion_blur(image, kernel_size = configs.BLUR_KERNEL_LOW, angle = configs.BLUR_ANLGE_HORIZONTAL):
     # angle 0: horizontal motion blur
     # angle 90: vertical motion blur
 
@@ -38,22 +40,72 @@ def apply_motion_blur(image, kernel_size = configs.BLUR_KERNEL_SIZE, angle = con
 
     blurred_image = cv2.filter2D(image, -1, motion_blur_kernel)
 
-    return Image.fromarray(blurred_image)
+    return blurred_image
 
-def main():
-    os.makedirs(configs.DEGRADED_NOISE_DIR, exist_ok=True)
-    os.makedirs(configs.DEGRADED_BLUR_DIR, exist_ok=True)
+def create_degraded_dataset():
+    parser = argparse.ArgumentParser(description="Create degraded dataset.")
+    parser.add_argument('--type', required=True, type=str,
+        choices=['noise_low', 'noise_med', 'noise_high', 'blur_low_hor', 'blur_low_ver', 'blur_med_hor', 'blur_med_ver', 'blur_high_hor', 'blur_high_ver'],
+        help="The type of degradation to apply."
+    )
+    args = parser.parse_args()
 
-    img1 = custom_pil_imread(f"{configs.ORIGINAL_IMG_DIR}/000000000776.png")
-    img2 = custom_pil_imread(f"{configs.ORIGINAL_IMG_DIR}/000000000785.png")
+    if args.type == 'noise_low':
+        output_dir = configs.DEGRADED_NOISE_LOW_DIR
+    elif args.type == 'noise_med':
+        output_dir = configs.DEGRADED_NOISE_MED_DIR
+    elif args.type == 'noise_high':
+        output_dir = configs.DEGRADED_NOISE_HIGH_DIR
+    elif args.type == 'blur_low_hor':
+        output_dir = configs.DEGRADED_BLUR_LOW_HOR_DIR
+    elif args.type == 'blur_low_ver':
+        output_dir = configs.DEGRADED_BLUR_LOW_VER_DIR
+    elif args.type == 'blur_med_hor':
+        output_dir = configs.DEGRADED_BLUR_MED_HOR_DIR
+    elif args.type == 'blur_med_ver':
+        output_dir = configs.DEGRADED_BLUR_MED_VER_DIR
+    elif args.type == 'blur_high_hor':
+        output_dir = configs.DEGRADED_BLUR_HIGH_HOR_DIR
+    elif args.type == 'blur_high_ver':
+        output_dir = configs.DEGRADED_BLUR_HIGH_VER_DIR
+    print(f"output dir:{output_dir}")
+    os.makedirs(output_dir, exist_ok=True)
 
-    add_gaussian_noise(img1, configs.MEAN, configs.SIGMA_LOW).save(f'{configs.DEGRADED_NOISE_DIR}/000000000776.png', format= 'PNG')
-    add_gaussian_noise(img2, configs.MEAN, configs.SIGMA_MED).save(f'{configs.DEGRADED_NOISE_DIR}/000000000785.png', format= 'PNG')
+    input_dir = configs.ORIGINAL_IMG_DIR
+    original_image_paths = glob.glob(os.path.join(input_dir, '*.png'))
 
-    apply_motion_blur(img1, configs.BLUR_KERNEL_SIZE, configs.BLUR_ANLGE_HORIZONTAL).save(f'{configs.DEGRADED_BLUR_DIR}/000000000776.png', format= 'PNG')
-    apply_motion_blur(img2, configs.BLUR_KERNEL_SIZE, configs.BLUR_ANGLE_VERTICAL).save(f'{configs.DEGRADED_BLUR_DIR}/000000000785.png', format= 'PNG')
+    print(f"\nFound {len(original_image_paths)} images to degrade.")
 
+    i = 1
+
+    for image_path in tqdm(original_image_paths, desc=f"Processing {args.type}."):
+        img = custom_pil_imread(image_path)
+
+        if args.type == 'noise_low':
+            degraded_img = add_gaussian_noise(img, configs.MEAN, configs.SIGMA_LOW)
+        elif args.type == 'noise_med':
+            degraded_img = add_gaussian_noise(img, configs.MEAN, configs.SIGMA_MED)
+        elif args.type == 'noise_high':
+            degraded_img = add_gaussian_noise(img, configs.MEAN, configs.SIGMA_HIGH)
+        elif args.type == 'blur_low_hor':
+            degraded_img = apply_motion_blur(img, configs.BLUR_KERNEL_LOW, configs.BLUR_ANLGE_HORIZONTAL)
+        elif args.type == 'blur_low_ver':
+            degraded_img = apply_motion_blur(img, configs.BLUR_KERNEL_LOW, configs.BLUR_ANGLE_VERTICAL)
+        elif args.type == 'blur_med_hor':
+            degraded_img = apply_motion_blur(img, configs.BLUR_KERNEL_MED, configs.BLUR_ANLGE_HORIZONTAL)
+        elif args.type == 'blur_med_ver':
+            degraded_img = apply_motion_blur(img, configs.BLUR_KERNEL_MED, configs.BLUR_ANGLE_VERTICAL)
+        elif args.type == 'blur_high_hor':
+            degraded_img = apply_motion_blur(img, configs.BLUR_KERNEL_HIGH, configs.BLUR_ANLGE_HORIZONTAL)
+        elif args.type == 'blur_high_ver':
+            degraded_img = apply_motion_blur(img, configs.BLUR_KERNEL_HIGH, configs.BLUR_ANGLE_VERTICAL)
+
+        base_filename = os.path.basename(image_path)
+        output_path = os.path.join(output_dir, base_filename)
+        Image.fromarray(degraded_img).save(output_path, format = 'PNG')
+
+    print("\n--- Full dataset degradation complete! ---")
     return
 
 if __name__ == '__main__':
-    main()
+    create_degraded_dataset()
